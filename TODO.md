445c76e8710353bcb03d12a8f0ade6a2652678a8
Gavin <gaprice@lbl.gov>
2019-11-15 14:21:56 -0800

    Cleanup SDK cruft, start on spec

---
+++ TODO.md
@@ -0,0 +1,3 @@
+* coverage
+* https://github.com/kbaseIncubator/samples/pull/1/files
+* The ~10 documents that have been written about this
--- lib/SampleService/SampleServiceImpl.py
+++ lib/SampleService/SampleServiceImpl.py
@@ -14,6 +14,8 @@ class SampleService:
 
     Module Description:
     A KBase module: SampleService
+
+Handles creating, updating, retriving samples and linking data to samples.
     '''
 
     ######## WARNING FOR GEVENT USERS ####### noqa
@@ -24,7 +26,7 @@ class SampleService:
     ######################################### noqa
     VERSION = "0.0.1"
     GIT_URL = "https://github.com/mrcreosote/sample_service.git"
-    GIT_COMMIT_HASH = "3bac7cdfc9fe72b5a8b807eb80248c32e2b7929b"
+    GIT_COMMIT_HASH = "c63c061ffbc6f2aef594a7fbdcd07badd8bf4c2a"
 
     #BEGIN_CLASS_HEADER
     #END_CLASS_HEADER
@@ -41,32 +43,55 @@ class SampleService:
         pass
 
 
-    def run_SampleService(self, ctx, params):
+    def create_sample(self, ctx, sample):
         """
-        This example function accepts any number of parameters and returns results in a KBaseReport
-        :param params: instance of mapping from String to unspecified object
-        :returns: instance of type "ReportResults" -> structure: parameter
-           "report_name" of String, parameter "report_ref" of String
+        Create a new sample or a sample version.
+        If Sample.id is null, a new Sample is created along with a new ID.
+        Otherwise, a new version of Sample.id is created. If Sample.id does not exist, an error
+          is returned.
+        :param sample: instance of type "Sample" (A Sample, consisting of a
+           tree of subsamples and replicates. id - the ID of the sample.
+           single_node - the node in a single node tree.) -> structure:
+           parameter "id" of type "sample_id" (A Sample ID. Must be globally
+           unique. Always assigned by the Sample service.), parameter
+           "single_node" of type "SampleNode" (A node in a sample tree. id -
+           the ID of the node. meta_controlled - metadata restricted by the
+           sample controlled vocabulary and validators. meta_user -
+           unrestricted metadata.) -> structure: parameter "id" of type
+           "node_id" (A SampleNode ID. Must be unique within a Sample,
+           contain only the characters [a-zA-Z0-9_], and be less than 255
+           characters.), parameter "meta_controlled" of type "metadata"
+           (Metadata attached to a sample.) -> mapping from type
+           "metadata_key" (A key in a metadata key/value pair. Less than 1000
+           unicode characters.) to tuple of size 2: type "metadata_value" (A
+           value in a metadata key/value pair. Less than 1000 unicode
+           characters.), type "units" (Units for a quantity, e.g. km, mol/g,
+           ppm, etc. 'None' for unitless quantities. Less than 50 unicode
+           characters.), parameter "meta_user" of type "metadata" (Metadata
+           attached to a sample.) -> mapping from type "metadata_key" (A key
+           in a metadata key/value pair. Less than 1000 unicode characters.)
+           to tuple of size 2: type "metadata_value" (A value in a metadata
+           key/value pair. Less than 1000 unicode characters.), type "units"
+           (Units for a quantity, e.g. km, mol/g, ppm, etc. 'None' for
+           unitless quantities. Less than 50 unicode characters.)
+        :returns: instance of type "SampleAddress" (A Sample ID and version.
+           id - the ID of the sample. version - the version of the sample.)
+           -> structure: parameter "id" of type "sample_id" (A Sample ID.
+           Must be globally unique. Always assigned by the Sample service.),
+           parameter "version" of type "version" (The version of a sample.
+           Always > 0.)
         """
         # ctx is the context object
-        # return variables are: output
-        #BEGIN run_SampleService
-        report = KBaseReport(self.callback_url)
-        report_info = report.create({'report': {'objects_created':[],
-                                                'text_message': params['parameter_1']},
-                                                'workspace_name': params['workspace_name']})
-        output = {
-            'report_name': report_info['name'],
-            'report_ref': report_info['ref'],
-        }
-        #END run_SampleService
+        # return variables are: address
+        #BEGIN create_sample
+        #END create_sample
 
         # At some point might do deeper type checking...
-        if not isinstance(output, dict):
-            raise ValueError('Method run_SampleService return value ' +
-                             'output is not type dict as required.')
+        if not isinstance(address, dict):
+            raise ValueError('Method create_sample return value ' +
+                             'address is not type dict as required.')
         # return the results
-        return [output]
+        return [address]
     def status(self, ctx):
         #BEGIN_STATUS
         returnVal = {'state': "OK",
