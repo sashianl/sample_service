ccb6aaf11139af547a2875276a99858753451491
Gavin <gaprice@lbl.gov>
2019-11-27 14:21:06 -0800

    Limit sample name length

--- lib/SampleService/core/sample.py
+++ lib/SampleService/core/sample.py
@@ -3,26 +3,28 @@ Contains classes related to samples.
 '''
 
 from uuid import UUID
-from SampleService.core.arg_checkers import not_falsy
+from typing import Optional
+from SampleService.core.arg_checkers import not_falsy, check_string
 
 # for now we'll assume people are nice and don't change attributes after init.
 # if that doesn't hold true, override __setattr__.
 
 
-# TODO test
+_MAX_SAMPLE_NAME_LEN = 255
+
 
 class Sample:
     '''
     A sample containing biological replicates, technical replicates, and sub samples.
     '''
 
-    def __init__(self, name: str = None):
+    def __init__(self, name: Optional[str] = None):
         '''
         Create the the sample.
-        :param name: The name of the sample.
+        :param name: The name of the sample. Cannot contain control characters or be longer than
+            255 characters.
         '''
-        # TODO restrictions on name
-        self.name = name
+        self.name = check_string(name, 'name', max_len=_MAX_SAMPLE_NAME_LEN, optional=True)
 
 
 class SampleWithID(Sample):
@@ -30,11 +32,12 @@ class SampleWithID(Sample):
     A sample including an ID.
     '''
 
-    def __init__(self, id_: UUID, name: str = None):
+    def __init__(self, id_: UUID, name: Optional[str] = None):
         '''
         Create the sample.
         :param id_': The ID of the sample.
-        :param name: The name of the sample.
+        :param name: The name of the sample. Cannot contain control characters or be longer than
+            255 characters.
         '''
         super().__init__(name)
         self.id = not_falsy(id_, 'id_')
