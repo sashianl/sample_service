4c55b33333b35611360adf6c753a102ed078e27e
Gavin <gaprice@lbl.gov>
2019-12-20 09:47:25 -0800

    Add auth controller
    Tested that it starts up correctly via a curl while the test was running.

--- test/SampleService_test.py
+++ test/SampleService_test.py
@@ -5,6 +5,9 @@ from pytest import fixture
 
 from core import test_utils
 from mongo_controller import MongoController
+from auth_controller import AuthController
+
+_AUTH_DB = 'test_auth_db'
 
 
 def create_deploy_cfg(mongo_port):
@@ -32,22 +35,43 @@ def create_deploy_cfg(mongo_port):
 
 
 @fixture(scope='module')
-def mongo():
+def temp_file():
+    tempdir = test_utils.get_temp_dir()
+    yield tempdir
+
+    if test_utils.get_delete_temp_files():
+        shutil.rmtree(test_utils.get_temp_dir())
+
+
+@fixture(scope='module')
+def mongo(temp_file):
     mongoexe = test_utils.get_mongo_exe()
     tempdir = test_utils.get_temp_dir()
     wt = test_utils.get_use_wired_tiger()
     mongo = MongoController(mongoexe, tempdir, wt)
-    print('running mongo {}{} on port {} in dir {}'.format(
-        mongo.db_version, ' with WiredTiger' if wt else '', mongo.port, mongo.temp_dir))
+    wttext = ' with WiredTiger' if wt else ''
+    print(f'running mongo {mongo.db_version}{wttext} on port {mongo.port} in dir {mongo.temp_dir}')
 
     yield mongo
 
     del_temp = test_utils.get_delete_temp_files()
-    print('shutting down mongo, delete_temp_files={}'.format(del_temp))
+    print(f'shutting down mongo, delete_temp_files={del_temp}')
     mongo.destroy(del_temp)
-    if del_temp:
-        shutil.rmtree(test_utils.get_temp_dir())
 
 
-def test_fake(mongo):
+@fixture(scope='module')
+def auth(mongo):
+    jd = test_utils.get_jars_dir()
+    tempdir = test_utils.get_temp_dir()
+    auth = AuthController(jd, f'localhost:{mongo.port}', _AUTH_DB, tempdir)
+    print(f'running KBase Auth2 {auth.version} on port {auth.port} in dir {auth.temp_dir}')
+
+    yield auth
+
+    del_temp = test_utils.get_delete_temp_files()
+    print(f'shutting down auth, delete_temp_files={del_temp}')
+    auth.destroy(del_temp)
+
+
+def test_fake(auth):
     pass
