3f3646463a30474e21f951834d5ae7f05c93e9ef
Gavin <gaprice@lbl.gov>
2020-07-13 13:46:50 -0700

    bump version

--- lib/SampleService/SampleServiceImpl.py
+++ lib/SampleService/SampleServiceImpl.py
@@ -52,7 +52,7 @@ Note that usage of the administration flags will be logged by the service.
     # state. A method could easily clobber the state set by another while
     # the latter method is running.
     ######################################### noqa
-    VERSION = "0.1.0-alpha19"
+    VERSION = "0.1.0-alpha20"
     GIT_URL = "https://github.com/mrcreosote/sample_service.git"
     GIT_COMMIT_HASH = "546f40c25082915554759778425080946081f4db"
 
