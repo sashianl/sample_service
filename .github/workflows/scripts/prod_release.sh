2e426074366c6f421dc87e1cdaf8741b538cb85f
Jason S Fillman <github@jasonfillman.com>
2022-02-23 18:22:19 -0800

    Testing branch check restrictions

--- .github/workflows/scripts/prod_release.sh
+++ .github/workflows/scripts/prod_release.sh
@@ -6,6 +6,7 @@ export DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export COMMIT=$(echo "$SHA" | cut -c -7)
 
+echo ${GITHUB_REF##*/}
 echo $DOCKER_TOKEN | docker login ghcr.io -u $DOCKER_ACTOR --password-stdin
 docker build --build-arg BUILD_DATE="$DATE" \
              --build-arg COMMIT="$COMMIT" \
