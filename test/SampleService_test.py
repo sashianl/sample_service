08ae0b5da79d1b3c2124d1b31d47df15b39cd80a
Charles Trenholm <charleshtrenholm@gmail.com>
2022-02-24 15:30:15 -0800

    fix tests, ensure acls come back in right order

--- test/SampleService_test.py
+++ test/SampleService_test.py
@@ -3314,7 +3314,7 @@ def test_get_links_from_sample_set_fail(sample_port):
             'sample_ids': [{'id': str(badid), 'version': 1}],
             'effective_time': _get_current_epochmillis() - 500
         },
-        'Sample service error code 50010 No such sample:' +
+        'Sample service error code 50010 No such sample:'
         f" Could not complete search for samples: ['{badid}']")
 
     # admin tests
